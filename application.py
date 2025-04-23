384c5baf962ad087a95b7403ee0ccc0037dcccd2
nicbot23 <n.ibarra@uniandes.edu.co>
2025-04-23 14:58:43 -0500

    cambio drastico 1

--- application.py
+++ application.py
@@ -38,8 +38,10 @@ def create_app(config_class=None):
     return application
 
 
-application = create_app()
-#print(f"\n🔗 DATABASE URI en runtime: {application.config['SQLALCHEMY_DATABASE_URI']}\n")
+application = None
+
+if __name__ == "__main__":
+    application = create_app()
 
 # if __name__ == "__main__":
 #     application.run(host="0.0.0.0", port=5000)
