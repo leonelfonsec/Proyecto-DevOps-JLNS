502a94cdb867eee0a621dd652ba8d6c148ad5ef5
nicbot23 <n.ibarra@uniandes.edu.co>
2025-04-23 17:01:19 -0500

    cambio application.py

--- application.py
+++ application.py
@@ -38,7 +38,8 @@ def create_app(config_class=None):
     return application
 
 
-application = create_app()
+
 
 if __name__ == "__main__":
+    application = create_app()
     application.run(host="0.0.0.0", port=5000)
