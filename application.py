ff18b254681b6ff52dde32a4d29540611df62f17
nicbot23 <n.ibarra@uniandes.edu.co>
2025-04-23 16:22:44 -0500

    cambios para dockerrun

--- application.py
+++ application.py
@@ -38,10 +38,7 @@ def create_app(config_class=None):
     return application
 
 
-application = None
+application = create_app()
 
 if __name__ == "__main__":
-    application = create_app()
-
-# if __name__ == "__main__":
-#     application.run(host="0.0.0.0", port=5000)
+    application.run(host="0.0.0.0", port=5000)
