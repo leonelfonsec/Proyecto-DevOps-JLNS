b15137c583142995f35ef5c482a2aefc8d48670b
nicbot23 <n.ibarra@uniandes.edu.co>
2025-04-23 00:16:36 -0500

    se movio application por no encontrar app

--- application.py
+++ application.py
@@ -1,4 +1,8 @@
 from app import create_app
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+print(sys.path)
 
 application = create_app()
 #print(f"\n🔗 DATABASE URI en runtime: {application.config['SQLALCHEMY_DATABASE_URI']}\n")
