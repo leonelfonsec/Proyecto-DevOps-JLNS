29d62bc822a0cab673f2b1f22df427627fbaab2a
nicbot23 <n.ibarra@uniandes.edu.co>
2025-04-23 11:02:37 -0500

    cambios buildspec sin zip

--- application.py
+++ application.py
@@ -7,5 +7,5 @@ print(sys.path)
 application = create_app()
 #print(f"\n🔗 DATABASE URI en runtime: {application.config['SQLALCHEMY_DATABASE_URI']}\n")
 
-if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     application.run(host="0.0.0.0", port=5000)
