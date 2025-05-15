7826118f3687eb74aececd06348a8a93c6bc8ad9
Leonel Fonseca <n.fonsecao@uniandes.edu.co>
2025-05-15 16:59:14 -0500

    fixs

--- blacklist_service/tests/test_routes.py
+++ blacklist_service/tests/test_routes.py
@@ -4,7 +4,8 @@ from app.models import BlacklistEntry
 from app.schemas import BlacklistSchema
 from app.routes import STATIC_TOKEN
 from faker import Faker
-from application import create_app
+from run import create_app
+
 
 fake = Faker()
 
